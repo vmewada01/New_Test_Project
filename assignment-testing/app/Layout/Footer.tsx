ab674624fc6da6ead995f4245b5860942cfa6a61
Vishal <vishalmewada9826@gmail.com>
2024-03-04 15:44:49 +0530

    added the layout file

---
+++ assignment-testing/app/Layout/Footer.tsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+  return (
+    <div 
+    className="bg-yellow-400 flex justify-center align-middle items-center p-2"
+    >Footer</div>
+  )
+}
+
+export default Footer
---
+++ assignment-testing/app/page.tsx
@@ -0,0 +1,14 @@
+import Footer from "./Layout/Footer";
+import Header from "./Layout/Header";
+import Sider from "./Layout/Sider";
+
+export default function Home() {
+  return (
+    <div className="h-[100vh] bg-pink-400 ">
+    <Header />
+    <Sider />
+    <Footer />
+     
+    </div>
+  )
+}
